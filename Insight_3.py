c1cd93dee138b11d629b74aa9c64d60e9ccf4e0d
sammcmanus <sammcmanus>
2025-09-29 09:05:48 -0400

    Initial Documentation

---
+++ Insight_3.py
@@ -0,0 +1,5 @@
+# Insight #3: Player Efficiency and Its Correlation to Experience
+
+#Goal: Quantify how veteran NBA players with 7+ years of experience demonstrate higher efficiency compared to less experienced players, highlighting their value to team success through optimized roles and production.
+
+player_stats_DF.head()
